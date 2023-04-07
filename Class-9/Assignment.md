f10262244c0597d5cbff2a8a9772daac5e978383
S M Shamim <smshamimdotcom@gmail.com>
2023-04-07 11:04:21 +0600

    Class-9

---
+++ Class-9/Assignment.md
@@ -0,0 +1,16 @@
+1. Write a Python program to find maximum between two numbers.
+2. Write a Python program to find maximum between three numbers.
+3. Write a Python program to check whether a number is divisible by 5 and 50 or not.
+4. Write a Python program to check whether a number is negative, positive or zero.
+5. Write a Python program to check whether a number is even or odd.
+6. Write a Python program to check whether a year is leap year or not.
+7. Write a Python program to check whether a character is alphabet or not.
+8. Write a Python program to input any alphabet and check whether it is vowel or consonant.
+9. Write a Python program to input any character and check whether it is alphabet, digit or special character.
+10. Write a Python program to check whether a character is uppercase or lowercase alphabet.
+
+
+dict
+
+
+
---
+++ Class-9/main.py
@@ -0,0 +1,63 @@
+# Conditional Logic - Decision Making
+
+"""
+1. Nums of Sc.
+2. Comparison   / Membership / Identity  Operator
+"""
+
+# 01 If statement
+
+"""
+office_time = 9
+current_time = 8
+
+print(office_time == current_time)
+
+if office_time == current_time:
+   print("Go to Office")
+
+# 02.  if else statement
+
+num = 100
+
+if num > 10:
+    print("Num is greater than 10")
+else:
+    print("Num is less than 10 or Equal 10")
+
+#  03. Nested if Statement
+
+num = 1000
+
+if num > 10:
+    print(">")
+if num < 10:
+    print("<")
+if num == 10:
+    print("==")
+# else:
+#     print("=")
+"""
+
+#  04. If..elif..else
+
+num = 5
+
+if num == 5 or num == 10:
+    print("==")
+    print("==")
+    print("==")
+    print("==")
+    print("==")
+elif num > 10:
+    print(">")
+elif num == 5:
+    print("5")
+else:
+    print("<")
+"""
+1. Short Answer 
+2. Multiple Choice 
+3. Multi-selection Choice  
+4. Output 
+"""
