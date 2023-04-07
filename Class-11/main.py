d1241cef34b2a1a098df5238dc941217a2a1c42e
S M Shamim <smshamimdotcom@gmail.com>
2023-04-07 12:00:30 +0600

    Class-11

---
+++ Class-11/main.py
@@ -0,0 +1,23 @@
+"""
+
+1. For Loop
+2. While Loop
+
+"""
+# While Loop
+start = 1
+end = 5 # Input()
+
+sum = 0
+
+while start <= end: # start >= end
+
+    num = int(input("Enter Number: "))
+    sum += num
+
+    start = start + 2
+
+print(sum)
+
+
+
