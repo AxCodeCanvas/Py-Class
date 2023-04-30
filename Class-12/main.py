f0e342609011b4f4a6057a65aeda67da05064cd4
S M Shamim <smshamimdotcom@gmail.com>
2023-05-01 00:03:24 +0600

    Class-12

---
+++ Class-12/main.py
@@ -0,0 +1,25 @@
+"""
+1. Empty List - Declare
+2. How many data want to add in the list ? -> input()
+3. [Loop] -> Input and add in the list
+
+[10, 130, 30, 50]
+
+# Is  Divisible by 10 ?  Print
+# 120 >
+
+"""
+
+lst = [10, 2, 40, 350]
+
+start = 0
+end = len(lst)
+
+while start < end: # 0 < 4 | 3
+
+    if lst[start] == 40:
+        break
+
+    print(lst[start])
+
+    start = 1
